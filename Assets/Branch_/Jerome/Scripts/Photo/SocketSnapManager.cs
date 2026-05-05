9aa6cb8ec6f58ad947e410172f0f4e6bb62d3d28
Jerome ENSI <j.dupret@nouvellesImages.xyz>
2026-05-05 12:10:05 +0200

    WIP : Photo Reconstruction

--- Assets/Branch_/Jerome/Scripts/Photo/SocketSnapManager.cs
+++ Assets/Branch_/Jerome/Scripts/Photo/SocketSnapManager.cs
@@ -1,40 +1,45 @@
+using System;
 using UnityEngine;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class SocketSnapHandler : MonoBehaviour
 {
-    public GameObject expectedPiece; // Which piece belongs here
+    public GameObject expectedPiece;
     private XRSocketInteractor socket;
-    [SerializeField] private bool isOccupied = false;
+    private bool isOccupied = false;
     private AssemblyManager manager;
     
     void Start()
     {
         socket = GetComponent<XRSocketInteractor>();
-        //manager = FindFirstObjectByType<AssemblyManager>();
+       // manager = FindObjectOfType<AssemblyManager>();
         socket.selectEntered.AddListener(OnSnapped);
     }
-    
+
     void OnSnapped(SelectEnterEventArgs args)
     {
+        Debug.Log("Viewport " + XRSettings.renderViewportScale);
         if (isOccupied) return;
         
         GameObject snappedPiece = args.interactableObject.transform.gameObject;
         
-        // Check if it's the right piece
-        if (snappedPiece != expectedPiece) return;
+        // Wrong piece - immediately eject
+        if (snappedPiece != expectedPiece)
+        {
+            // Force release the wrong piece
+            socket.interactionManager.SelectExit(args.interactorObject, args.interactableObject);
+            Rigidbody rb = args.interactableObject.transform.GetComponent<Rigidbody>();
+            rb.AddForce(args.interactableObject.transform.forward * 1, ForceMode.Impulse);
+            return;
+        }
         
+        // Correct piece - accept it
         isOccupied = true;
-        
-        // Disable grabbing on the piece
         snappedPiece.GetComponent<XRGrabInteractable>().enabled = false;
-        
-        // Disable this socket
         socket.enabled = false;
-        
-        // Notify manager
-        //manager.PiecePlaced();
+       // manager.PiecePlaced();
     }
 }
