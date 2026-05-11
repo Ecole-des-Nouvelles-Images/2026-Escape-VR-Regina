c812b97084383313223d97dd7bdd6f159558d0ba
Ritail <loan.jourdan@gmail.com>
2026-05-11 16:51:03 +0200

    Add_HintController_Fix_PuzzleSequenceManager

--- Assets/Branch_/Loan/Scripts/HintController.cs
+++ Assets/Branch_/Loan/Scripts/HintController.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class HintController : MonoBehaviour
 {
@@ -12,11 +13,21 @@ public class HintController : MonoBehaviour
    [SerializeField]  private float _timeToPuzzleChange;
    [Header("===== Hint Object =====")]
    [SerializeField] private GameObject _hint;
+   [SerializeField] private GameObject _hintGrab;
+   [SerializeField] private Transform _hintGrabParent;
 
+   private Vector3 _startHintGrabPause = new Vector3();
+   private Rigidbody _hintGrabRb;
+   private XRGrabInteractable _grab;
+   
    private bool _isRunning = true;
 
    private void Start()
    {
+      _grab = GetComponentInChildren<XRGrabInteractable>();
+      _startHintGrabPause = _hintGrab.transform.position;
+      _hintGrabRb = _hintGrab.GetComponent<Rigidbody>();
+      
       EventBus.OnPuzzleChanged += PuzzleChanged;
       EventBus.OnHintGived += GivenHint;
       _currentTimeToNextHint = _timeToPuzzleChange;
@@ -32,8 +43,7 @@ public class HintController : MonoBehaviour
    {
       _currentTime = 0;
       _currentTimeToNextHint = _timeToPuzzleChange;
-      _hint.SetActive(false);
-      _isRunning = true;
+      Hide();
    }
 
    private void GivenHint()
@@ -44,8 +54,7 @@ public class HintController : MonoBehaviour
       string hint = PuzzleSequenceManager.Instance.GiveStringHint();
       Debug.Log(hint);
       
-      _hint.SetActive(false);
-      _isRunning = true;
+      Hide();
    }
 
    private void Update()
@@ -61,4 +70,27 @@ public class HintController : MonoBehaviour
          _isRunning = false;
       }
    }
+   
+   public void Hide()
+   {
+      ForceRelease();
+
+      _hintGrab.transform.position = _startHintGrabPause;
+      _hintGrabRb.angularVelocity = Vector3.zero;
+      _hintGrabRb.linearVelocity = Vector3.zero;
+      _hint.SetActive(false);
+      _isRunning = true;
+   }
+
+   private void ForceRelease()
+   {
+      if (!_grab.isSelected)
+         return;
+
+      var interactor = _grab.firstInteractorSelecting;
+
+      _grab.interactionManager.SelectExit(interactor, _grab);
+      _hintGrab.transform.SetParent(_hintGrabParent);
+   }
 }
+
