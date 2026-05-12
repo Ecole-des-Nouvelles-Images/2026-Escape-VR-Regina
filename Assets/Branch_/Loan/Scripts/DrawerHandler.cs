1c3023c4ec7bebb17b4762662e5ca11b7e54af00
Ritail <loan.jourdan@gmail.com>
2026-05-12 14:43:24 +0200

    Tweak_Drawer_MoveBySocket

--- Assets/Branch_/Loan/Scripts/DrawerHandler.cs
+++ Assets/Branch_/Loan/Scripts/DrawerHandler.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class DrawerHandler : Puzzle
 {
    [SerializeField] private GameObject _drawer01;
    [SerializeField] private GameObject _drawer02;
-   [SerializeField] private XRGrabInteractable _grabDrawer;
+   [SerializeField] private XRGrabInteractable _grabDrawer; 
+   [SerializeField]private XRSocketInteractor  _grabSocket;
+   [SerializeField]private XRSocketInteractor  _grabSocket1;
 
    private bool _isEnabled;
 
    private void Awake()
    {
       _grabDrawer.enabled = false;
+      _grabSocket.selectEntered.AddListener(OnSocket);
+      _grabSocket1.selectEntered.AddListener(OnSocket1);
    }
 
    private void Update()
@@ -27,6 +33,22 @@ public class DrawerHandler : Puzzle
          Solve();
       }
          
+   }
+
+   private void OnSocket(SelectEnterEventArgs args)
+   {
+      _drawer01.gameObject.SetActive(true);
+      GameObject insertedObject = args.interactableObject.transform.gameObject;
+      Destroy(insertedObject);
+      _grabSocket.gameObject.SetActive(false);
+   }
+   
+   private void OnSocket1(SelectEnterEventArgs args)
+   {
+      _drawer02.gameObject.SetActive(true);
+      GameObject insertedObject = args.interactableObject.transform.gameObject;
+      Destroy(insertedObject);
+      _grabSocket1.gameObject.SetActive(false);
    }
 
    public override void Solve()
