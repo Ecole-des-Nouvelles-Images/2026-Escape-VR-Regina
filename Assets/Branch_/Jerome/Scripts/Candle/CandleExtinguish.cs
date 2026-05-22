a926f1aad5d3fba84fea9f36e36bb4bb9cdd16ca
Jerome ENSI <j.dupret@nouvellesImages.xyz>
2026-05-22 12:08:05 +0200

    Minor Feat : Candle extinguish
    Changed it to work with the shader of Sasha so that it turns off her boolean that makes it disapear.

--- Assets/Branch_/Jerome/Scripts/Candle/CandleExtinguish.cs
+++ Assets/Branch_/Jerome/Scripts/Candle/CandleExtinguish.cs
@@ -1,42 +1,39 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Renderer))]
 public class CandleExtinguish : MonoBehaviour
 {
-    [SerializeField] private Material _flameMaterial;
-    [SerializeField] private string _flameActiveProperty = "_FlameActive";
+    [SerializeField] private string _flameActiveProperty;
     [SerializeField] private ParticleSystem _extinguish;
     
     private bool _isExtinguished = false;
     private Collider _candleCollider;
-    private Material _flameMaterialInstance;
+    private Renderer _candleRenderer;
+    private Material _flameMaterial;
     private int _flamePropertyId;
     private CandleManager _candleManager;
 
     private void Start()
     {
-        // Get the collider component
+        // Get components
         _candleCollider = GetComponent<Collider>();
+        _candleRenderer = GetComponent<Renderer>();
         
-        // Find the CandleManager in the scene
-        _candleManager = FindAnyObjectByType<CandleManager>();
-        
-        // Create instance of the flame material to avoid modifying the prefab
-        if (_flameMaterial)
+        // Get the existing material (automatically becomes an instance when modified)
+        if (_candleRenderer)
         {
-            _flameMaterialInstance = new Material(_flameMaterial);
+            _flameMaterial = _candleRenderer.material; // This creates a unique instance automatically
             _flamePropertyId = Shader.PropertyToID(_flameActiveProperty);
-            
-            // Apply the material instance to the renderer
-            Renderer renderer = GetComponent<Renderer>();
-            if (renderer) renderer.material = _flameMaterialInstance;
         }
         else
         {
-            Debug.LogError("Flame material not assigned to CandleExtinguish!");
+            Debug.LogError("Renderer not found on candle!");
         }
         
-        // Register this candle with the manager
+        // Find the CandleManager
+        _candleManager = FindAnyObjectByType<CandleManager>();
+        
         if (_candleManager)
         {
             _candleManager.RegisterCandle(this);
@@ -49,7 +46,6 @@ public class CandleExtinguish : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the entering object has the tag "CandleSnuffer" and the candle isn't already extinguished
         if (other.CompareTag("CandleSnuffer") && !_isExtinguished)
         {
             ExtinguishCandle();
@@ -58,39 +54,26 @@ public class CandleExtinguish : MonoBehaviour
 
     private void ExtinguishCandle()
     {
-        // Turn off the flame via shader property (0 = off, 1 = on)
-        if (_flameMaterialInstance)
+        // Turn off the flame via shader property
+        if (_flameMaterial)
         {
-            _flameMaterialInstance.SetFloat(_flamePropertyId, 0f);
+            _flameMaterial.SetFloat(_flamePropertyId, 0f);
         }
         
-        // Set the boolean to true
         _isExtinguished = true;
         
-        // Turn off the collider
         if (_candleCollider) _candleCollider.enabled = false;
         
-        // Play the one-shot extinguish VFX
         if (_extinguish)
         {
             _extinguish.Play();
             Destroy(_extinguish.gameObject, _extinguish.main.duration);
         }
         
-        // Notify the manager that this candle has been extinguished
         if (_candleManager) _candleManager.CandleExtinguished(this);
         
         Debug.Log($"Candle {gameObject.name} extinguished!");
     }
     
     public bool GetIsExtinguished() => _isExtinguished;
-    
-    private void OnDestroy()
-    {
-        // Clean up the material instance to prevent memory leaks
-        if (_flameMaterialInstance)
-        {
-            Destroy(_flameMaterialInstance);
-        }
-    }
 }
